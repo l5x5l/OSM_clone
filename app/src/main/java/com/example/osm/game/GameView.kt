2bad971d9f4b1dc47522b95f7f5a33599c68f1b7
YSH <sh10302@naver.com>
2021-07-22 12:51:22 +0900

    make 4 direction note

--- app/src/main/java/com/example/osm/game/GameView.kt
+++ app/src/main/java/com/example/osm/game/GameView.kt
@@ -6,17 +6,28 @@ import android.util.Log
 import android.view.SurfaceView
 import com.example.osm.R
 
+import kotlin.collections.ArrayList
+import kotlin.random.Random
+
+
 class GameView(context : Context, screenX : Int, screenY : Int) : SurfaceView(context), Runnable{
 
     private lateinit var thread : Thread
     private var isPlaying = false
     private val screenX = screenX
     private val screenY = screenY
-    private val noteList = ArrayList<Note>()
     private val trashNoteList = ArrayList<Note>()
     private val paint = Paint()
-
     private var count = 0
+    private var noteSize = 200
+    private val speed = 30
+    // 순서대로 위, 오른쪽, 아래, 왼쪽 노트
+    private val noteList = listOf<ArrayList<Note>>(ArrayList<Note>(), ArrayList<Note>(), ArrayList<Note>(), ArrayList<Note>())
+    private val positionList = listOf<IntArray>(intArrayOf((screenX - noteSize) / 2, 0 - noteSize), intArrayOf(screenX, (screenY - noteSize) / 2),
+            intArrayOf((screenX - noteSize) / 2, screenY), intArrayOf(0 - noteSize, (screenY - noteSize) / 2))
+    private val noteSpeedList = listOf<IntArray>(intArrayOf(0, speed), intArrayOf(-speed, 0), intArrayOf(0, -speed), intArrayOf(speed, 0))
+
+
 
     override fun run() {
         while(isPlaying){
@@ -27,19 +38,27 @@ class GameView(context : Context, screenX : Int, screenY : Int) : SurfaceView(co
     }
 
     private fun update() {
-        if (count % 9 == 0)
-            newNote(0)
-
-        for (note in noteList) {
-            note.y += 30
-            if (note.y  >= screenY / 2 - note.height / 2){
-                //noteList.remove(note)
-                trashNoteList.add(note)
-            }
+        if (count % 9 == 0){
+            val temp = Random.nextInt(4)
+            newNote(temp)
         }
 
-        for (note in trashNoteList){
-            noteList.remove(note)
+        for (OneDirectionNoteList in noteList){
+            for (note in OneDirectionNoteList) {
+
+                val temp = note.getDirection()
+
+                note.x += noteSpeedList[temp][0]
+                note.y += noteSpeedList[temp][1]
+                if (check(temp, note.y, note.x)){
+                    //noteList.remove(note)
+                    trashNoteList.add(note)
+                }
+            }
+
+            for (note in trashNoteList){
+                OneDirectionNoteList.remove(note)
+            }
         }
 
         trashNoteList.clear()
@@ -50,9 +69,12 @@ class GameView(context : Context, screenX : Int, screenY : Int) : SurfaceView(co
         if(holder.surface.isValid){
             var canvas = holder.lockCanvas()
             canvas.drawColor(resources.getColor(R.color.white))
-            for(note in noteList){
-                //drawColor를 이쪽에 넣으면 이전 2프레임이 반복된다, 왜?
-                canvas.drawBitmap(note.note, note.x.toFloat(), note.y.toFloat(), paint)
+
+            for (OneDirectionNoteList in noteList){
+                for(note in OneDirectionNoteList){
+                    //drawColor를 이쪽에 넣으면 이전 2프레임이 반복된다, 왜?
+                    canvas.drawBitmap(note.note, note.x.toFloat(), note.y.toFloat(), paint)
+                }
             }
             holder.unlockCanvasAndPost(canvas)
         }
@@ -79,10 +101,20 @@ class GameView(context : Context, screenX : Int, screenY : Int) : SurfaceView(co
     }
 
     public fun newNote(direction : Int){
-        val note = Note(resources, direction)
-        note.x = screenX / 2 - note.width / 2
-        note.y = 0 - note.height
-        noteList.add(note)
+        val note = Note(resources, direction, noteSize)
+        note.x = positionList[direction][0] //screenX / 2 - note.width / 2
+        note.y = positionList[direction][1]
+        noteList[direction].add(note)
     }
+
+    private fun check(direction : Int, y : Int, x : Int) : Boolean{
+        return when (direction) {
+            0 -> { y >= (screenY  - noteSize) / 2 }
+            1 -> { x <= (screenX - noteSize) / 2 }
+            2 -> { y <= (screenY - noteSize) / 2 }
+            else -> { x >= (screenX - noteSize) / 2 }
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/osm/game/Note.kt
+++ app/src/main/java/com/example/osm/game/Note.kt
@@ -5,12 +5,17 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import com.example.osm.R
 
-class Note(resources: Resources, direction : Int) {
+class Note(resources: Resources, direction : Int, size : Int = 200) {
     private val direction = direction
     var x = 0
     var y = 0
     val note_bitmap: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.pngwing_com)
-    val note: Bitmap = Bitmap.createScaledBitmap(note_bitmap, note_bitmap.width/8, note_bitmap.height/8, false)
+    //note_bitmap.width/8
+    val note : Bitmap = Bitmap.createScaledBitmap(note_bitmap, size, size, false)
     val width = note.width
     val height = note.height
+
+    fun getDirection() : Int{
+        return direction
+    }
 }
